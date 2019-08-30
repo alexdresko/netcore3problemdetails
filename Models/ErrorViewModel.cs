da67f73c789d52b43917be5af36060ba009002a7
alexdresko <me@alexdresko.com>
2019-08-30 15:05:27 +0000

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hellangcore3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
